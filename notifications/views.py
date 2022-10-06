d88ab19c25202d943d92783c276df2f4e22ee12b
Sougata P <Sougata P>
2022-10-06 20:19:38 +0530

    updated views for notifications

--- notifications/views.py
+++ notifications/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render
 
-from .models import Configuration, NotificationLog
+from .models import NotificationLog
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .tasks import send_email_notification
 
-
 class NotificationView(APIView):
-
     def post(self, request):
-
         data = request.data
 
         # Create Notification
         notificationlog_obj = NotificationLog()
         notificationlog_obj.metadata = data
         notificationlog_obj.save()
+
+
 
         send_email_notification.delay(notificationlog_obj.id)
 
