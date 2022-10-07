e8b4e57c909d4507096bdeb6454e275781ef391a
Sougata P <Sougata P>
2022-10-07 11:55:27 +0530

    updated fixtures

--- notifier/settings.py
+++ notifier/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'notifier.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("DATABASE_NAME", "open_notifier"),
+        'USER': os.environ.get("DATABASE_USER", "postgres"),
+        'PASSWORD': os.environ.get("DATABASE_PASSWORD", "postgres"),
+        'HOST': os.environ.get("DATABASE_HOST", "localhost"),
+        'PORT': os.environ.get("DATABASE_PORT", 5432)
     }
 }
 
@@ -137,7 +140,7 @@ REST_FRAMEWORK = {
 
 # configure CELERY
 REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
-REDIS_PORT = int(os.environ.get("REDIS_PORT", 6379))
+REDIS_PORT = int(os.environ.get("REDIS_PORT", "6379"))
 REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", None)
 REDIS_CELERY_DB = 0
 if REDIS_PASSWORD is None:
