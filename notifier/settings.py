35507fb2799f4b3224b4d0145b51792ee388b087
Sougata P <Sougata P>
2023-04-14 11:15:32 +0530

    updated notifier middleware

--- notifier/settings.py
+++ notifier/settings.py
@@ -92,7 +92,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'pf.middlewares.tracer.TracerMiddleware'
+    'notifier.middlewares.tracer.TracerMiddleware'
 ]
 
 ROOT_URLCONF = 'notifier.urls'
