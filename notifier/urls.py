23358d824522bdfe2dc3ad86d2187f9c0493f36a
Sougata P <Sougata P>
2023-05-04 12:08:59 +0530

    formatting updated

--- notifier/urls.py
+++ notifier/urls.py
@@ -25,6 +25,12 @@ urlpatterns = [
     path("notifier/dev-console/", admin.site.urls),
     # notifications
     path("notifier/tenants/<tenant_id>/notifications", NotificationView.as_view()),
-    re_path(r"^notifier/media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
-    re_path(r"^notifier/static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+    re_path(
+        r"^notifier/media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}
+    ),
+    re_path(
+        r"^notifier/static/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.STATIC_ROOT},
+    ),
 ]
